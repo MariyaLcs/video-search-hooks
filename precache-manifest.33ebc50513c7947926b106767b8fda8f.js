95bd7115416250913ece4a76197b72ebd8400861
MariyaLcs <mariya.podosinova@gmail.com>
2020-10-12 15:04:32 +0100

    Updates

---
+++ precache-manifest.33ebc50513c7947926b106767b8fda8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f14fb2c303104bce0dc306364ff2956d",
+    "url": "/video-search-hooks/index.html"
+  },
+  {
+    "revision": "210ca0d944c528212818",
+    "url": "/video-search-hooks/static/css/main.8ad48a67.chunk.css"
+  },
+  {
+    "revision": "74380a36b64878743e76",
+    "url": "/video-search-hooks/static/js/2.52078bfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/video-search-hooks/static/js/2.52078bfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "210ca0d944c528212818",
+    "url": "/video-search-hooks/static/js/main.e14fde62.chunk.js"
+  },
+  {
+    "revision": "c67392833fe6786156c7",
+    "url": "/video-search-hooks/static/js/runtime-main.f2ed3700.js"
+  }
+]);
